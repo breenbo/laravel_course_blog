44a31820d779750dd0d4f71013b42a887734f71f
Bruno Berrehuel <bruno@homepad.ch>
2023-08-14 15:49:10 +0200

    Add dynamic individual post view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,17 +10,15 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\Category::factory(3)->create();
-        $user = User::factory()->create(
-            [
-            'name' => 'John Doe',
-            ]
-        );
+        $user = User::factory()->create();
 
         \App\Models\Post::factory(10)->create(
             [
